e2c9831f9872434bc9fbe58f34e2636e83f53f2a
IrisRainbowNeko <228218809@qq.com>
2024-08-08 15:55:04 +0800

    loss weight

--- mlneko/loss/asymmetric.py
+++ mlneko/loss/asymmetric.py
@@ -57,7 +57,9 @@ class AsymmetricLoss(nn.Module):
             loss *= one_sided_w
 
         if self.cls_weight is not None:
-            loss = loss * self.cls_weight
+            cls_weight_pos = y*self.cls_weight
+            cls_weight_neg = (1-y)*(1/self.cls_weight)
+            loss = loss * (cls_weight_pos+cls_weight_neg)
 
         return -loss.sum()/B
 
